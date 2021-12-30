781e1f5da4a8cca4554d8b88aded2dabefc3c95f
l3lackJack <gabrielionuturs@iesebre.com>
2021-12-30 19:13:43 +0100

    WIP

--- src/graficos/HojaSprites.java
+++ src/graficos/HojaSprites.java
@@ -3,13 +3,14 @@ package graficos;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
-
+//dafd5531-6836-4780-b83e-e8f2a23441d1
 public class HojaSprites {
     private final int ancho;
     private final int alto;
     public final int[] pixeles;
 
     //Coleccion de hojas de sprites:
+    //No funciona ruta:
     public static HojaSprites desierto = new HojaSprites("/texturas/desierto.png",320,320);
     //fin de la coleccion
 
